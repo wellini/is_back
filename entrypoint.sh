3eb31d347bc442ba0f7d79cccbcdfff312a97f11
Alexander <isanny314@gmail.com>
2020-04-23 14:53:17 +0300

    email-confirmation: (feature) Sending emails

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 mvn -Ddocker.env.postgres.jdbc.url=jdbc:postgresql://postgres:5432/${POSTGRES_USER} \
     -Ddocker.env.postgres.user=${POSTGRES_USER} \
     -Ddocker.env.postgres.password=${POSTGRES_PASSWORD} \
+    -Ddocker.env.mail.enable=${MAIL_ENABLE} \
+    -Ddocker.env.mail.host=${MAIL_HOST} \
+    -Ddocker.env.mail.port=${MAIL_PORT} \
+    -Ddocker.env.mail.user=${MAIL_USER} \
+    -Ddocker.env.mail.password=${MAIL_PASSWORD} \
     spring-boot:run
---
+++ src/main/java/com/nonfallable/taskKnight/services/ConfirmationByEmailService.java
@@ -0,0 +1,46 @@
+package com.nonfallable.taskKnight.services;
+
+import com.nonfallable.taskKnight.models.ConfirmationToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConfirmationByEmailService {
+
+    @Autowired
+    private JavaMailSender emailSender;
+
+    @Value("${docker.env.mail.enable}")
+    private boolean isMailingEnable;
+
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private String port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    public void sendConfirmationCode(ConfirmationToken token, String to) {
+        if(!isMailingEnable) return;
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        switch (token.getType()) {
+            case REGISTRATION:
+                message.setSubject("Confirm your registration – Task Knight");
+            case CHANGE_PASSWORD:
+                message.setSubject("Password change – Task Knight");
+            default:
+                message.setSubject("Your confirmation code – Task Knight");
+        }
+        message.setText("Hello! Your confirmation code is: " + token.getCode());
+        emailSender.send(message);
+    }
+}
