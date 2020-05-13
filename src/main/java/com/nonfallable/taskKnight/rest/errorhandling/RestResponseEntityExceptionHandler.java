86fe7c0fbb4a4a70c073d118b31c6ab6c5a996e7
wellini <40068963+wellini@users.noreply.github.com>
2020-05-13 23:49:29 +0300

    error400: (bugfix) Turn off banner, add 400 error handling (#12)
    Co-authored-by: Alexander <isanny314@gmail.com>

--- src/main/java/com/nonfallable/taskKnight/rest/errorhandling/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/nonfallable/taskKnight/rest/errorhandling/RestResponseEntityExceptionHandler.java
@@ -7,8 +7,10 @@ import com.nonfallable.taskKnight.security.ManagedSecurityException;
 import com.nonfallable.taskKnight.rest.dto.ApiErrorDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -50,5 +52,10 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
         logger.error("RestResponseEntityExceptionHandler", ex);
         ex.printStackTrace();
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiErrorDTO().setMessage("Internal server error :("));
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ApiErrorDTO().setMessage("Internal server error :("));
     }
 }
