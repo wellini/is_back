631548248b807545767cc3150e7fa9df740f4164
wellini <40068963+wellini@users.noreply.github.com>
2021-09-16 19:34:47 +0300

    Remove confirmation coode (#15)
    Co-authored-by: Alexander <isanny314@gmail.com>

--- src/main/java/com/nonfallable/taskKnight/rest/authentication/ChangePasswordRestFacade.java
+++ src/main/java/com/nonfallable/taskKnight/rest/authentication/ChangePasswordRestFacade.java
@@ -1,8 +1,6 @@
 package com.nonfallable.taskKnight.rest.authentication;
 
 import com.nonfallable.taskKnight.exceptions.NotFoundException;
-import com.nonfallable.taskKnight.models.ConfirmationToken;
-import com.nonfallable.taskKnight.models.ConfirmationTokenType;
 import com.nonfallable.taskKnight.models.PasswordChanging;
 import com.nonfallable.taskKnight.models.Profile;
 import com.nonfallable.taskKnight.repositories.PasswordChangingRepository;
@@ -13,8 +11,6 @@ import com.nonfallable.taskKnight.rest.authentication.dto.ConfirmationCodeReques
 import com.nonfallable.taskKnight.rest.authentication.dto.ConfirmationCodeResponseDTO;
 import com.nonfallable.taskKnight.rest.authentication.validators.ChangePasswordRequestValidator;
 import com.nonfallable.taskKnight.rest.authentication.validators.ConfirmationCodeRequestRequestValidator;
-import com.nonfallable.taskKnight.services.ConfirmationByEmailService;
-import com.nonfallable.taskKnight.services.ConfirmationTokenService;
 import com.nonfallable.taskKnight.services.ProfileService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -28,39 +24,33 @@ import static org.springframework.http.ResponseEntity.ok;
 @Component
 public class ChangePasswordRestFacade {
 
-    private ConfirmationTokenService confirmationTokenService;
     private ProfileService profileService;
     private ProfileRepository profileRepository;
     private ConfirmationCodeRequestRequestValidator confirmationCodeRequestRequestValidator;
     private ChangePasswordRequestValidator changePasswordRequestValidator;
     private PasswordChangingRepository passwordChangingRepository;
     private PasswordEncoder passwordEncoder;
-    private ConfirmationByEmailService confirmationByEmailService;
 
-    public ChangePasswordRestFacade(ConfirmationTokenService confirmationTokenService, ProfileService profileService, ProfileRepository profileRepository, ConfirmationCodeRequestRequestValidator confirmationCodeRequestRequestValidator, ChangePasswordRequestValidator changePasswordRequestValidator, PasswordChangingRepository passwordChangingRepository, PasswordEncoder passwordEncoder, ConfirmationByEmailService confirmationByEmailService) {
-        this.confirmationTokenService = confirmationTokenService;
+    public ChangePasswordRestFacade(ProfileService profileService, ProfileRepository profileRepository, ConfirmationCodeRequestRequestValidator confirmationCodeRequestRequestValidator, ChangePasswordRequestValidator changePasswordRequestValidator, PasswordChangingRepository passwordChangingRepository, PasswordEncoder passwordEncoder) {
         this.profileService = profileService;
         this.profileRepository = profileRepository;
         this.confirmationCodeRequestRequestValidator = confirmationCodeRequestRequestValidator;
         this.changePasswordRequestValidator = changePasswordRequestValidator;
         this.passwordChangingRepository = passwordChangingRepository;
         this.passwordEncoder = passwordEncoder;
-        this.confirmationByEmailService = confirmationByEmailService;
     }
 
     @Transactional
     public ResponseEntity<ChangePasswordResponseDTO> changePassword(ChangePasswordRequestDTO requestDTO) {
         changePasswordRequestValidator.validate(requestDTO);
         Profile profile = profileService.getAuthorizedUserProfile();
-        PasswordChanging passwordChanging = passwordChangingRepository.save(
+        passwordChangingRepository.save(
                 new PasswordChanging()
                         .setNewPassword(passwordEncoder.encode(requestDTO.getNewPassword()))
                         .setSubject(profile)
         );
-        ConfirmationToken confirmationToken = confirmationTokenService.createToken(passwordChanging.getId().toString(), ConfirmationTokenType.CHANGE_PASSWORD);
-        confirmationByEmailService.sendConfirmationCode(confirmationToken, profile.getEmail());
 
-        return ok(new ChangePasswordResponseDTO(confirmationToken.getId()));
+        return ok(new ChangePasswordResponseDTO(UUID.randomUUID()));
     }
 
     @Transactional
@@ -69,7 +59,6 @@ public class ChangePasswordRestFacade {
         Profile profile = profileService.getAuthorizedUserProfile();
         PasswordChanging passwordChanging = passwordChangingRepository.findBySubject(profile)
                 .orElseThrow(() -> new NotFoundException("Password changing record not found"));
-        confirmationTokenService.confirm(token, requestDTO.getCode(), passwordChanging.getId().toString(), ConfirmationTokenType.CHANGE_PASSWORD);
         profileRepository.save(profile.setPassword(passwordChanging.getNewPassword()));
         passwordChangingRepository.delete(passwordChanging);
         return ok(new ConfirmationCodeResponseDTO("OK"));
